8f829178e7f3fd380f646c102df76ca9eb13db67
taya-win <tayawin.tyw@gmail.com>
2024-08-21 20:28:18 +0630

    Chapter (47) - Laravel Authentication

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/articles', [ArticleController::class, 'index']);
 
 Route::get('/articles/detail/{id}', [ArticleController::class,'detail']);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
