fdab4e65f239a98de3d4f1f02650bdf8f3766463
taya-win <tayawin.tyw@gmail.com>
2024-08-21 19:43:32 +0630

    Chapter (42) - Laravel Routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles', function () {
+    return 'Article List';
+});
+
+Route::get('/articles/detail', function () {
+    return 'Article Detail';
+})->name('article.detail');
+
+
+Route::get('/articles/detail/{id}', function ( $id ) {
+    return "Article Detail - $id";
+});
+
+Route::get('/articles/more', function() {
+    return redirect()->route('article.detail');
+});
+
