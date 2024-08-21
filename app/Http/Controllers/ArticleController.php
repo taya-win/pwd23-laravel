0b93d582f5a96be6d1137f11b0750a891dd43c51
taya-win <tayawin.tyw@gmail.com>
2024-08-21 20:11:30 +0630

    Chapter (46) - Laravel Migration and Model

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $data = [
-            [ "id" => 1, "title" => "First Article" ],
-            [ "id" => 2, "title" => "Second Article" ],
-        ];
+        $data = Article::all();
 
         return view('articles.index', ['articles' => $data]);
     }
